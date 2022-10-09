509637f86cddc05dc1c058bd13c03cd26eb3328d
vicvv <xolosno270@gmail.com>
2022-10-08 19:28:57 -0700

    added comments

--- tries/multiStringSearch.py
+++ tries/multiStringSearch.py
@@ -1,4 +1,7 @@
-
+'''Write a function that takes in a big string and an array of small strings, all of which are smaller in length
+than the big string. The function should return an array of booleans, where each boolean represents
+whether the small string at that index in the array of small strings is contained in the big string.
+Note that you can't use language-built-in string-matching methods.'''
 
 
 
--- tries/suffixTryConstruction.py
+++ tries/suffixTryConstruction.py
@@ -1,7 +1,13 @@
-# Do not edit the class below except for the
-# populateSuffixTrieFrom and contains methods.
-# Feel free to add new properties and methods
-# to the class.
+
+'''Suffix Trie Construction
+Write a SuffixTrie class for a Suffix-Trie-like data structure. The class should have a root property
+set to be the root node of the trie and should support:
+Creating the trie from a string; this will be done by calling the populateSuffixTrieFrom method
+upon class instantiation, which should populate the root of the class.
+Searching for strings in the trie.
+Note that every string added to the trie should end with the special endSymbol character: "*" .
+If you're unfamiliar with Suffix Tries, we recommend watching the Conceptual Overview section of this
+question's video explanation before starting to code.'''
 class SuffixTrie:
     def __init__(self, string):
         self.root = {}
